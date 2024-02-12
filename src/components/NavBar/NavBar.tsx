4a996cfa581816ff9bd3bcfe4b1cfc4ba4fc4493
Carlos A. Wong <carloswong54@gmail.com>
2024-02-11 21:36:04 -0500

    Added meilisearch logo to nav bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { BiShoppingBag } from 'react-icons/bi';
 import { SearchBox } from 'react-instantsearch';
+import MeilisearchLogo from '@/public/meilisearch-logo.svg'
 import styles from '@/styles/nav-bar.module.css';
 
 export default function NavBar() {
   return (
     <div className={styles.container}>
+      <Link href="/" style={{ width: '125px' }}>
+        <Image src={MeilisearchLogo} width={75} alt="logo" />
+      </Link>
       <SearchBox />
+      <button className={styles.btn}>
+        <BiShoppingBag /> <span>0</span>
+      </button>
     </div>
   );
 };

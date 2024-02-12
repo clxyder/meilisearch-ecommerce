a0d8c9366b05ed82eb32ee4edb34b78867cc1477
Carlos A. Wong <carloswong54@gmail.com>
2024-02-11 21:29:01 -0500

    Working e-commerce site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import InstantSearchLayout from "@/components/InstantSearchLayout/InstantSearchLayout";
 import "@/styles/globals.css";
+import "@/styles/searchBoxAIS.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/components/RatingMenu/RatingMenu.tsx
+++ src/components/RatingMenu/RatingMenu.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from 'react';
 import { useConnector } from 'react-instantsearch';
 import connectRatingMenu from 'instantsearch.js/es/connectors/rating-menu/connectRatingMenu';
